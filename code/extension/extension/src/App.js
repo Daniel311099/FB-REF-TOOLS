40dc4007b4e4d856a257dd8ac9008346d3a7ffca
Daniel311099 <danielfisaha@gmail.com>
2022-06-08 19:25:43 +0100

    extension

---
+++ code/extension/extension/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  console.log('works')
+  return (
+    <div className="App">
+      Hello Again 2
+    </div>
+  );
+}
+
+export default App;
